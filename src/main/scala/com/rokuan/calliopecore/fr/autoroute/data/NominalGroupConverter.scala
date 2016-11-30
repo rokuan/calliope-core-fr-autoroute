34d219be2e3c7ba648e66b1c481c9d59658089db
LEBEAU Christophe <c.lebeau23@gmail.com>
2016-11-30 22:35:06 +0100

    new rule for the subject in NominalGroup and new rule for the PlaceAdverbial

--- src/main/scala/com/rokuan/calliopecore/fr/autoroute/data/NominalGroupConverter.scala
+++ src/main/scala/com/rokuan/calliopecore/fr/autoroute/data/NominalGroupConverter.scala
@@ -1,7 +1,9 @@
 package com.rokuan.calliopecore.fr.autoroute.data
 
 import com.rokuan.calliopecore.fr.sentence.Word
+import com.rokuan.calliopecore.sentence.INameInfo
 import com.rokuan.calliopecore.sentence.structure.data.count.CountObject
+import com.rokuan.calliopecore.sentence.structure.data.nominal.UnitObject.UnitType
 import com.rokuan.calliopecore.sentence.structure.data.nominal._
 
 /**
@@ -16,31 +18,52 @@ object NominalGroupConverter {
   /*val AbstractTarget = word(POSSESSIVE_ADJECTIVE) {
 
   }*/
+  val NumberTransformer = word(NUMBER) { _.getValue.toInt }
+  val DoubleTransformer = word(NUMBER) { _.getValue.toDouble }
+  val UnitTransformer = word(UNIT) { _.getUnitInfo.getUnitType }
 
   val FirstName = list(word(FIRSTNAME))
   val LastName = list(word(PROPER_NAME))
 
+  val CommonObjectTransformer = word(COMMON_NAME) { _.getNameInfo }
+
+  val SimpleObject = (CountConverter.CountRule ~ CommonObjectTransformer) {
+    case List(count: CountObject, name: INameInfo) =>
+      new NameObject {
+        count = count
+        `object` = name
+      }
+  }
+  val PhoneNumber = (word(DEFINITE_ARTICLE) ~ list(NumberTransformer)) {
+    case List(_, numbers: List[Int]) =>
+      // TODO: How to handle numbers such as "099" ?
+      val phoneNumber = numbers.map {
+        case n if n < 100 => String.format("%2d", n)
+        case n => n.toString
+      }.mkString
+      new PhoneNumberObject {
+        number = phoneNumber
+      }
+  }
+  val Quantity = (DoubleTransformer ~ UnitTransformer) {
+    case List(quantity: Int, unit: UnitType) =>
+      new QuantityObject {
+        amount = quantity
+        unitType = unit
+      }
+  }
   val Color = (word(DEFINITE_ARTICLE) ~ word(COLOR)) {
     case List(_, c: Word) =>
       new ColorObject { color = c.getColorInfo }
   }
-
-  val CommonName = (CountConverter.CountRule ~ word(COMMON_NAME)) {
-    case List(count: CountObject, n: Word) =>
-      new NameObject {
-        count = count
-        `object` = n.getNameInfo
-      }
-  }
-
   val AdditionalPerson = word(PERSON) { w => new AdditionalPerson { person = w.getCustomPerson } }
-
   val Language = (word(DEFINITE_ARTICLE) ~ word(LANGUAGE)) {
     case List(_, l: Word) =>
       new LanguageObject { language = l.getLanguageInfo }
   }
-
   val DirectObject = opt(CountConverter.CountRule) ~ word(COMMON_NAME)
-
   val Pronoun = word(PERSONAL_PRONOUN) { w => new PronounSubject(sentence.Pronoun.parseSubjectPronoun(w.getValue))}
+
+  val SubjectRule = Quantity | PhoneNumber | PlaceConverter.City | PlaceConverter.Country |
+    Pronoun | DateConverter.FixedDate | Color | Language | SimpleObject
 }
--- src/main/scala/com/rokuan/calliopecore/fr/autoroute/data/PlaceConverter.scala
+++ src/main/scala/com/rokuan/calliopecore/fr/autoroute/data/PlaceConverter.scala
@@ -1,8 +1,44 @@
 package com.rokuan.calliopecore.fr.autoroute.data
 
+import com.rokuan.calliopecore.fr.sentence.Word
+import com.rokuan.calliopecore.sentence.{ICityInfo, ICountryInfo}
+import com.rokuan.calliopecore.sentence.structure.data.nominal.{CityObject, CountryObject}
+import com.rokuan.calliopecore.sentence.structure.data.place.LocationObject
+import com.rokuan.calliopecore.sentence.structure.data.place.PlaceAdverbial.PlaceType
+
 /**
   * Created by Christophe on 27/11/2016.
   */
-class PlaceConverter {
+object PlaceConverter {
+  import com.rokuan.calliopecore.fr.sentence.Word.WordType._
+  import com.rokuan.calliopecore.fr.autoroute.pattern.WordRules._
+  import com.rokuan.calliopecore.fr.autoroute.pattern.PrepositionRules._
+  import com.rokuan.autoroute.rules.Rule._
 
+
+  val CityTransformer = word(CITY) { _.getCityInfo }
+  val CountryTransformer = word(COUNTRY) { _.getCountryInfo }
+  val CityPrepositionTransformer = (prep(PlaceType.CITY) ~ opt(word(DEFINITE_ARTICLE))) {
+    case List(preposition: Word, _) => preposition.getPlacePreposition
+  } | prep(PlaceType.CITY, true) { _.getPlacePreposition }
+  val CountryPrepositionTransformer = (prep(PlaceType.COUNTRY) ~ opt(word(DEFINITE_ARTICLE))) {
+    case List(preposition: Word, _) => preposition.getPlacePreposition
+  } | prep(PlaceType.COUNTRY, true) { _.getPlacePreposition }
+
+  val City = CityTransformer { c =>
+    new CityObject { city = c }
+  }
+
+  val Country = CountryTransformer { c =>
+    new CountryObject { country = c }
+  }
+
+  val WorldLocation =
+    (CityPrepositionTransformer ~ CityTransformer ~ CountryPrepositionTransformer ~ CountryTransformer) {
+      case List(_, city: ICityInfo, _, country: ICountryInfo) =>
+        new LocationObject {
+          city = city
+          country = country
+        }
+    }
 }
